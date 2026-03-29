66cc8cba0b4cb8ec5238768ec44a205f4eed2155
M07amedm0usa <155415556+M07amedm0usa@users.noreply.github.com>
2026-03-29 23:50:59 +0200

    Refactor RemotionRoot to use MyVideo component
    Updated the RemotionRoot component to use MyVideo instead of HelloWorld and adjusted dimensions.

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,45 +1,20 @@
-import { Composition } from "remotion";
-import { HelloWorld, myCompSchema } from "./HelloWorld";
-import { Logo, myCompSchema2 } from "./HelloWorld/Logo";
-
-// Each <Composition> is an entry in the sidebar!
+import { Composition } from 'remotion';
+import { MyVideo } from './MyVideo';
+import data from '../public/assets/data.json';
 
 export const RemotionRoot: React.FC = () => {
+  const sceneDuration = 150; // لازم يكون نفس الرقم اللي في MyVideo.tsx
+  const totalDuration = data.length * sceneDuration;
+
   return (
     <>
       <Composition
-        // You can take the "id" to render a video:
-        // npx remotion render HelloWorld
-        id="HelloWorld"
-        component={HelloWorld}
-        durationInFrames={150}
+        id="HelloWorld" // لازم نفس الاسم اللي في render.yml
+        component={MyVideo}
+        durationInFrames={totalDuration}
         fps={30}
-        width={1920}
-        height={1080}
-        // You can override these props for each render:
-        // https://www.remotion.dev/docs/parametrized-rendering
-        schema={myCompSchema}
-        defaultProps={{
-          titleText: "Welcome to Remotion",
-          titleColor: "#000000",
-          logoColor1: "#91EAE4",
-          logoColor2: "#86A8E7",
-        }}
-      />
-
-      {/* Mount any React component to make it show up in the sidebar and work on it individually! */}
-      <Composition
-        id="OnlyLogo"
-        component={Logo}
-        durationInFrames={150}
-        fps={30}
-        width={1920}
-        height={1080}
-        schema={myCompSchema2}
-        defaultProps={{
-          logoColor1: "#91dAE2" as const,
-          logoColor2: "#86A8E7" as const,
-        }}
+        width={1080}
+        height={1920}
       />
     </>
   );

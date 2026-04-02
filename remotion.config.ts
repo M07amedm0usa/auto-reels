46a7d8151e830b5a09eeb55824234d50b34b62b7
M07amedm0usa <155415556+M07amedm0usa@users.noreply.github.com>
2026-04-02 17:42:50 +0200

    Delete remotion.config.ts

--- remotion.config.ts
+++
@@ -1,6 +0,0 @@
-import { Config } from '@remotion/cli/config';
-
-Config.setVideoImageFormat('jpeg');
-Config.setOverwriteOutput(true);
-Config.setConcurrency(1); // لو الـ server صغير — زوده لو عندك CPU أكتر
-Config.setChromiumOpenGlRenderer('swiftshader'); // مهم جداً في GitHub Actions (headless)
